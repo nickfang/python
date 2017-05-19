1e29c9704f14965124016abfedf1adb41a4c9e4b
nfang <fang.nicholas@gmail.com>
2017-05-19 12:13:42 -0500

    Finally got it working.  A couple of things were throwing me off.  I forgot to always return the retValue since I need to keep the list of nodes that have been traversed.  I should have realized this when I kept getting return values of None.  I also should not have been checking for a node value of None, since only children that exist are called with: for itr in node.children:

--- problems/trees/multiChildTreeSearch.py
+++ problems/trees/multiChildTreeSearch.py
@@ -16,6 +16,7 @@ class Node():
 			self._printNodes(itr)
 		print("")
 
+	# makes class work with the print() function
 	def __str__(self):
 		return str(self.value)
 
@@ -39,8 +40,6 @@ e.children.append(g)
 a.printTree()
 
 # create a function that will return the node that contains the value that is passed in.
-# return the path of nodes it took to get to that node
-
 def findNode(node, k):
 	if node == None:
 		return
@@ -52,26 +51,33 @@ def findNode(node, k):
 			if retValue != None and retValue.value == k:
 				return retValue
 
-#TODO: finish this problem.  for some reason, 2 works, but 3 does not.
+# create a function that will return the node that contains the value that is passed in.
+# return the path of nodes it took to get to that node
 def findNodeWithPath(node, k):
-	if node == None:
-		return []
+	# if node == None:
+	# 	return []
+	retValue = []
 	if node.value == k:
 		return [node]
 	else:
+		# if there's no children, this shouldn't run
 		for itr in node.children:
 			retValue = findNodeWithPath(itr, k)
-			if retValue == []:
-				return []
-			elif retValue[0].value == k:
-				return [retValue] + [node]
-			else:
-				return retvalue + [node]
+			if len(retValue) > 0:
+				return [node] + retValue
+		return retValue
+
+# run functions
+for i in range(8)[1:]:
+	if findNode(a, i).value == i:
+		print("Node with value {:d} found".format(i))
+	else:
+		print("NODE NOT FOUND!!")
 
 for i in range(8)[1:]:
-	print("Value of node found: ", findNode(a, i))
+	print(i, ": ", end="")
 	for itr in findNodeWithPath(a, i):
-		if itr != None:
-			print(itr, end=" ")
-
-	print("")
+		print(itr.value, end=" ")
+		# if itr != None:
+		# 	print(itr, end=" ")
+	print()
